a620efbd3616b4c03a861fc8e431abb1550c6554
Lissandrodev <Luismexico2006@gmail.com>
2025-08-30 16:37:31 -0700

    V3.0

--- js/script.js
+++ js/script.js
@@ -26,62 +26,38 @@ function actualizarSalida() {
         mostrar.textContent = '0';
     }
 }
+
+/*
+* nota, al mometo de cambiar estilos mediante el jaavscriopt este lo hace mediante los etilos inline,
+* lo que nos puede traer problemas al tener estilos externos, haciedo que no agarren o no funcionen
+* debido a la cascada de estilos, tieane mas preferencia los inline que los de la hoja externa
+* por lo cual solo debemos de utilizar important en casos muy necesarios para evitar colisiones
+* */
+function theme(option) {
+    switch (option) {
+        case 1:
+            document.body.classList.remove('tema-principal','tema-blanco','tema-purpura')
+            document.body.classList.add('tema-principal');
+            break;
+        case 2:
+            document.body.classList.remove('tema-principal','tema-blanco','tema-purpura')
+
+            document.body.classList.add('tema-blanco') ;
+            break;
+        case 3:
+            document.body.classList.remove('tema-principal','tema-blanco','tema-purpura')
+
+            document.body.classList.add('tema-purpura') ;
+            break;
+    }
+}
+
 // Funciones extras relacionadas con la apariencia
 let delee = document.querySelector('.del');
 delee.addEventListener('click', () => {
     document.querySelector('#showResult').textContent = "0";
 });
-let tema1 = document.querySelector(".bg-normal")
-tema1.addEventListener("click", () => {
-    document.body.style.backgroundColor = "#3A4764FF";
-    document.querySelector('#showResult').style.color = '';
-    document.querySelector(".bg-keypad").style.backgroundColor = 'hsl(223, 31%, 20%)';
-    document.querySelector(".contenedor__display").style.backgroundColor = 'hsl(224, 36%, 15%)';
-    document.querySelector("#lastres").style.color = 'white';
-    document.querySelector("#showResult").style.color = 'white';
-    document.querySelector("#title").style.color = 'hsl(60, 10%, 19%)';
-    document.querySelector("#equal").style.backgroundColor = 'hsl(6, 63%, 50%)';
-    document.querySelector("#reset").style.backgroundColor = 'hsl(225, 21%, 49%)';
-    document.querySelector("#del").style.backgroundColor = 'hsl(225, 21%, 49%)';
-    document.querySelector("#title").style.color = 'white';
 
-    alert("Tema cambiado!");
-});
-let tema2 = document.querySelector(".bg-white");
-tema2.addEventListener("click", () => {
-    document.body.style.backgroundColor = "#E6E6E6FF";
-    document.querySelector('#showResult').style.color = '';
-    document.querySelector(".bg-keypad").style.backgroundColor = 'hsl(0, 5%, 81%)';
-    document.querySelector(".contenedor__display").style.backgroundColor = 'hsl(0, 0%, 93%)';
-    document.querySelector("#lastres").style.color = 'hsl(60, 10%, 19%)';
-    document.querySelector("#showResult").style.color = 'hsl(60, 10%, 19%)';
-    document.querySelector("#title").style.color = 'hsl(60, 10%, 19%)';
-    document.querySelector("#equal").style.backgroundColor = 'hsl(25, 98%, 40%)';
-    document.querySelector("#reset").style.backgroundColor = ' hsl(185, 42%, 37%)';
-    document.querySelector("#del").style.backgroundColor = ' hsl(185, 42%, 37%)';
-
-    alert("Tema cambiado!");
-});
-let tema3 = document.querySelector(".bg-purple")
-
-tema3.addEventListener("click", () => {
-    document.body.style.backgroundColor = "#160628FF";
-    document.querySelector('#showResult').style.color = '';
-    document.querySelector(".bg-keypad").style.backgroundColor = 'hsl(268,71%,12%)';
-    document.querySelector(".contenedor__display").style.backgroundColor = 'hsl(268,71%,12%)';
-    document.querySelector("#lastres").style.color = 'hsl(52, 100%, 62%)';
-    document.querySelector("#showResult").style.color = 'hsl(52, 100%, 62%)';
-    document.querySelector("#equal").style.backgroundColor = 'hsl(176, 100%, 44%)';
-    document.querySelector("#reset").style.backgroundColor = 'hsl(285, 91%, 52%)';
-    document.querySelector("#del").style.backgroundColor = 'hsl(285,91%,52%)';
-    document.querySelector("#title").style.color = 'hsl(52, 100%, 62%)';
-    document.querySelectorAll(".num").forEach(num => {
-        num.style.backgroundColor = 'hsl(268, 47%, 21%)';
-        num.style.color = 'hsl(52, 100%, 62%)';
-        num.style.boxShadow = '0 2px 0 1px hsl(290, 70%, 36%)';
-    });
-    alert("Tema cambiado!");
-});
 function del() {
     // Metodo del string que elimina un caracter de izquierda a derecha
     mostrar.textContent = mostrar.textContent.slice(0, -1);
